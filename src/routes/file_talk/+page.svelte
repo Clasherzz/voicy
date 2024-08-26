a408bc54e45325792844c4a491d8d71c85516845
Clasherzz <govindkm20044@gmail.com>
2024-08-26 12:55:02 +0530

    adding file support

---
+++ src/routes/file_talk/+page.svelte
@@ -0,0 +1,27 @@
+
+<script>
+    function parseFile() {
+            const fileInput = document.getElementById('fileInput');
+         //   const output = document.getElementById('output');
+            const file = fileInput.files[0];
+
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(event) {
+                    const fileContent = event.target.result;
+                  //  output.textContent = fileContent;
+                };
+
+                reader.onerror = function(event) {
+                   // output.textContent = "Error reading file!";
+                   console.log("error here");
+                };
+
+                reader.readAsText(file); // Reads the file as plain text
+            } else {
+                output.textContent = "No file selected!";
+            }
+        }
+</script>
+<input type="file" id="fileInput">
+<!-- <button onclick="parseFile()">Upload and Parse</button> -->
